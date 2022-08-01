04b59980b9e2e60809b401de1f0d75083c7a750a
GSoumyaReddy <106742830+GSoumyaReddy@users.noreply.github.com>
2022-08-01 17:42:03 +0530

    Add files via upload

---
+++ src/components/AddUserForm.js
@@ -0,0 +1,157 @@
+import React,{useState} from 'react'
+//import { Link } from 'react-router-dom';
+import {useDispatch} from 'react';
+import { addUser } from '../actions/user';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+export default function AddUserForm(props){
+   
+const dispatch = useDispatch();
+   /* const [id,setId]=useState(0);
+   const [name,setName]=useState('')
+   const [brand,setBrand]=useState('')
+   const [price,setPrice]=useState(0); */
+
+const initialFormState = {
+    userLoginId:0,
+    userName:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    contact:'',
+    email:''
+}
+ 
+const [user,setUser]=useState(initialFormState);
+//a function which is universal for all changes 
+//id ,name ,brand , price
+//when is handleInputChange 
+//when you are changing the name of the product
+//      event.target.name =name
+//event.target.value ='mouse'
+
+
+
+const handleInputChange = (event)=>{
+   const {name,value} =event.target;
+  
+   setUser({...user,[name]:value});
+}
+ 
+/*
+const handleIdChange =(event)=>{
+   setId(event.target.value)
+}
+
+const handleNameChange = (event)=>{
+   setName(event.target.value)
+}
+
+const handleBrandChange=(event)=>{
+   setBrand(event.target.value)
+}
+
+const handlePriceChange=(event)=>{
+   setPrice(event.target.value)
+}*/
+
+const submitHandler=(event)=>{event.preventDefault();
+if( !user.userLoginId || !user.userName || !user.password||!user.firstName||!user.lastName||
+    !user.contact||!user.email) return;
+ console.log(user+'from adduserform')
+//props.addUser(user);
+dispatch(addUser(user));
+setUser(initialFormState);
+
+}
+return (
+  <Card style={{ backgroundColor:"lightGrey",width:'30rem'}}>
+    <ListGroup variant="flush">
+  
+   <Form onSubmit={submitHandler} >
+     <Form.Group className="mb-3" controlId="formBasicuserLoginId" >
+       <Form.Label>userLoginId</Form.Label>
+       <Form.Control type="number" placeholder="userLoginId" required
+        name='userLoginId'
+        value={user.userLoginId}
+       onChange={handleInputChange} />
+       
+     </Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasicUserName" >
+  <Form.Label>userName</Form.Label>
+
+  <Form.Control type="Name" placeholder="userName" required
+
+       name='userName'
+       value={user.userName}
+       onChange={handleInputChange} />
+
+</Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasicPassword">
+     
+     <Form.Label>password</Form.Label>
+     
+       <Form.Control type="password" placeholder="password" required
+
+       name='password'
+       value={user.password}
+       onChange={handleInputChange} />
+
+
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      
+     </Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasicFirstName">
+       <Form.Label>firstName</Form.Label>
+       <Form.Control type="text"  placeholder="FirstName" required
+        name='firstName'
+        value={user.firstName}
+       onChange={handleInputChange} />
+     </Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasiLastName">
+       <Form.Label>lastName</Form.Label>
+       <Form.Control type="text"  placeholder="LastName" required
+         name='lastName'
+         value={user.lastName}
+       onChange={handleInputChange} />
+     </Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasicContact">
+       <Form.Label>contact</Form.Label>
+       <Form.Control type="text"  placeholder="ContactNo" required
+         name='contact'
+         value={user.contact}
+       onChange={handleInputChange} />
+     </Form.Group>
+
+     <Form.Group className="mb-3" controlId="formBasiEmail">
+       <Form.Label>email</Form.Label>
+       <Form.Control type="text"  placeholder="name@email.com" required
+         name='email'
+         value={user.email}
+       onChange={handleInputChange} />
+     </Form.Group>
+     
+     <Button variant="primary" type="submit">
+
+      submit
+
+
+      
+     </Button>
+   </Form>
+
+   </ListGroup>
+   </Card>
+ );
+}
---
+++ src/components/EditUserForm.js
@@ -0,0 +1,89 @@
+import React , {useContext, useEffect, useState} from 'react'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+export default function EditUserForm(props){
+     const [user,setUser] =useState(props.currentUser)
+
+    const handleInputChange = (event)=>{
+        const {name,value} =event.target;
+       
+        setUser({...user,[name]:value});
+     }
+
+
+     const submitHandler=(event)=>{event.preventDefault();
+       props.updateUser(user.userLoginId,user);
+    }
+
+
+   
+
+     return (
+        <form onSubmit={submitHandler}>
+
+         <Card style={{ width: '30rem' }}>
+      
+      <Card.Body>
+        <Card.Title>Edit User form</Card.Title>
+         
+<label>userLoginId</label>
+<h1>{props.currentUser.userLoginId}</h1>
+
+<label>userName</label>
+<input 
+type='text'
+name='userName'
+value={user.userName}
+onChange={handleInputChange}/>
+
+<label>password</label>
+<input 
+type='text'
+name='password'
+value={user.password}
+onChange={handleInputChange}/>
+
+<label>firstName</label>
+<input 
+type='text'
+name='firstName'
+value={user.firstName}
+onChange={handleInputChange}/>
+
+<label>lastName</label>
+<input 
+type='text'
+name='lastName'
+value={user.lastName}
+onChange={handleInputChange}/>
+
+<label>contact</label>
+<input 
+type='number'
+name='contact'
+value={user.contact}
+onChange={handleInputChange}/>
+
+<label>email</label>
+<input 
+type='text'
+name='email'
+value={user.email}
+onChange={handleInputChange}/>
+
+<button>Update User</button>
+<button onClick={()=>props.setEditing(false)} 
+className="button muted-button">Cancel</button>
+</Card.Body>
+</Card>
+
+</form>
+
+
+   );
+
+
+
+
+}
